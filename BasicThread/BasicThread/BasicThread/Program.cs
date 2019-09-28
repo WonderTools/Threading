ac475dd8062d0fe7d8ad22c3054c6e02c8ba16b5
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-09-28 10:37:24 +0530

    Some basic coding

---
+++ BasicThread/BasicThread/BasicThread/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicThread
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(() => { Print("s");});
+            thread.Name = "thread 1";
+            thread.IsBackground = true;
+
+            Thread thread2 = new Thread(() => { Print("*************"); });
+            //thread2.Name = "thread 2";
+            thread2.IsBackground = true;
+
+
+            thread.Start();
+
+            thread2.Start();
+
+            Print("Good");
+
+            //thread2.Join();
+            //thread.Join();
+
+        }
+
+        static void Print(string s)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"Printing {i} -> {s} -> {Thread.CurrentThread.ManagedThreadId}");
+                //Thread.Sleep(300);
+            }
+        }
+    }
+}
