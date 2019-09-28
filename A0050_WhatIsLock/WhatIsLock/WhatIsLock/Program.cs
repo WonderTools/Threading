ae3756df81bf0d8620cec09dd4ca5dc18d84be2f
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-09-28 14:22:57 +0530

    Lock is done

---
+++ A0050_WhatIsLock/WhatIsLock/WhatIsLock/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading;
+
+namespace WhatIsLock
+{
+    class Program
+    {
+        static object obj = new object();
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => PrintUpTriangleNTimes(5, 10));
+            Thread t2 = new Thread(() => PrintDownTriangleNTimes(5, 10));
+
+            //Always keep one option commented
+            //Option1(t1, t2);
+            Option2(t1, t2);
+
+        }
+
+        private static void Option1(Thread t1, Thread t2)
+        {
+            t1.Start();
+            t1.Join();
+            t2.Start();
+        }
+
+        private static void Option2(Thread t1, Thread t2)
+        {
+            t1.Start();
+            t2.Start();
+        }
+
+        private static void PrintDownTriangle(int length)
+        {
+            lock (obj)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    for (int j = i; j < length; j++)
+                    {
+                        Console.Write("o");
+                        MinorDelay();
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+
+            MinorDelay();
+        }
+
+
+        private static void PrintUpTriangleNTimes(int length, int n)
+        {
+            for (int i = 0; i < n; i++) PrintUpTriangle(length);
+        }
+
+        private static void PrintDownTriangleNTimes(int length, int n)
+        {
+            for (int i = 0; i < n; i++) PrintDownTriangle(length);
+        }
+
+        private static void PrintUpTriangle(int length)
+        {
+            lock (obj)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write(".");
+                        MinorDelay();
+                    }
+                    Console.WriteLine();
+                }
+            }
+            MinorDelay();
+        }
+
+        private static void MinorDelay()
+        {
+            int j = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                j++;
+            }
+        }
+    }
+}
