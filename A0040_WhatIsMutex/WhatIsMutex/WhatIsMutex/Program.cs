e830a938122dba3df7c6d9c62b543d18d2d96b77
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-09-28 14:15:48 +0530

    Mutex

---
+++ A0040_WhatIsMutex/WhatIsMutex/WhatIsMutex/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+
+namespace WhatIsMutex
+{
+    class Program
+    {
+        static Mutex _mutex = new Mutex();
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => PrintUpTriangleNTimes(5, 10));
+            Thread t2 = new Thread(() => PrintDownTriangleNTimes(5, 10));
+
+            //Always keep one option commented
+            //Option1(t1, t2);
+            Option2(t1, t2);
+
+        }
+
+        private static void Option1(Thread t1, Thread t2)
+        {
+            t1.Start();
+            t1.Join();
+            t2.Start();
+        }
+
+        private static void Option2(Thread t1, Thread t2)
+        {
+            t1.Start();
+            t2.Start();
+        }
+
+        private static void PrintDownTriangle(int length)
+        {
+            _mutex.WaitOne();
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i; j < length; j++)
+                {
+                    Console.Write("o");
+                    MinorDelay();
+                }
+
+                Console.WriteLine();
+            }
+            _mutex.ReleaseMutex();
+        }
+
+
+        private static void PrintUpTriangleNTimes(int length, int n)
+        {
+            for (int i = 0; i < n; i++) PrintUpTriangle(length);
+        }
+
+        private static void PrintDownTriangleNTimes(int length, int n)
+        {
+            for (int i = 0; i < n; i++) PrintDownTriangle(length);
+        }
+
+        private static void PrintUpTriangle(int length)
+        {
+            _mutex.WaitOne();
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(".");
+                    MinorDelay();
+                }
+                Console.WriteLine();
+            }
+            _mutex.ReleaseMutex();
+        }
+
+        private static void MinorDelay()
+        {
+            int j = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                j++;
+            }
+        }
+    }
+}
