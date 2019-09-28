6ea85283a83996dacba6b3e47aca377d02f9fb29
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-09-28 11:18:46 +0530

    Completed What is background thread

---
+++ A0020_WhatIsBackgroundThread/WhatIsBackgroundThread/WhatIsBackgroundThread/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+
+namespace WhatIsBackgroundThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var url = "https://www.google.com";
+
+            Thread thread1 = new Thread(LogEvery5Seconds);
+            thread1.IsBackground = true;
+            thread1.Start();
+
+            Thread thread = new Thread(new ThreadStart(() => { PingWebPage10Times(url); }));
+            thread.Start();
+            
+        }
+
+        private static void LogEvery5Seconds()
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine($"Approximately {i * 5 } seconds elapsed since start of the program");
+                Thread.Sleep(5000);
+                i++;
+            }
+        }
+
+        private static void PingWebPage10Times(string url)
+        {
+            HttpClient client = new HttpClient();
+            for (int i = 0; i < 10; i++)
+            {
+                var responseMessage = client.GetAsync(url).Result;
+                Console.WriteLine($"S.No. {i}: The response for https://google.com is {responseMessage.StatusCode}");
+                Thread.Sleep(4000);
+            }
+            client.Dispose();
+
+        }
+    }
+}
